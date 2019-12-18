2739fe07d640b736784599f49c1d3b7762cd19ab
Christian Hadiwinoto <chrhad@users.noreply.github.com>
2019-12-18 23:16:54 +0800

    Update runall_s1.bash
    Link to experimental settings on Table 2 of the paper

--- runall_s1.bash
+++ runall_s1.bash
@@ -5,7 +5,7 @@ MODELS_DIR=models/AW
 
 mkdir -p ${MODELS_DIR}
 for SD in 111 222 123; do
-
+# 1. Simple (1sent+1sur)
 python train.py --bert-model bert-base-cased --dropout 0 --optimizer bert-adam --layer 11 \
     --num-context 1 --seed ${SD} \
     --devset-path ${WSD_DATA_DIR}/semeval2007/semeval2007.data.xml \
@@ -16,7 +16,7 @@ python train.py --bert-model bert-base-cased --dropout 0 --optimizer bert-adam -
 for TESTSET in semeval2007 senseval2 senseval3 semeval2013 semeval2015; do
     bash test_postproc.sh ${TESTSET} ${MODELS_DIR}/semcor_enbert_s1_sd${SD}_model
 done
-
+# 2. LW (1sent+1sur)
 python train.py --bert-model bert-base-cased --dropout 0 --optimizer bert-adam \
     --num-context 1 --seed ${SD} \
     --devset-path ${WSD_DATA_DIR}/semeval2007/semeval2007.data.xml \
@@ -27,7 +27,7 @@ python train.py --bert-model bert-base-cased --dropout 0 --optimizer bert-adam \
 for TESTSET in semeval2007 senseval2 senseval3 semeval2013 semeval2015; do
     bash test_postproc.sh ${TESTSET} ${MODELS_DIR}/semcor_lwa_enbert_s1_sd${SD}_model
 done
-
+# 3. GLU (1sent+1sur)
 python train.py --bert-model bert-base-cased --dropout 0.1 --optimizer bert-adam --layer 11 \
     --num-context 1 --use-glu --residual-glu --seed ${SD} \
     --devset-path ${WSD_DATA_DIR}/semeval2007/semeval2007.data.xml \
@@ -38,7 +38,7 @@ python train.py --bert-model bert-base-cased --dropout 0.1 --optimizer bert-adam
 for TESTSET in semeval2007 senseval2 senseval3 semeval2013 semeval2015; do
     bash test_postproc.sh ${TESTSET} ${MODELS_DIR}/semcor_glur_do0.1_enbert_s1_sd${SD}_model
 done
-
+# 4. LW+GLU (1sent+1sur)
 python train.py --bert-model bert-base-cased --dropout 0.1 --optimizer bert-adam \
     --num-context 1 --use-glu --residual-glu --seed ${SD} \
     --devset-path ${WSD_DATA_DIR}/semeval2007/semeval2007.data.xml \
